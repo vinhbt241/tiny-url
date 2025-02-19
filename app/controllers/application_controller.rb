b1b09bc0fd8352515ad2292dc7b551f2cbb42bb2
vinhbt241 <vinhbuith32412000@gmail.com>
2025-02-19 22:00:08 +0700

    add exeption handlers for clear error response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   allow_browser versions: :modern
+
+  include ExceptionFilter
 end
---
+++ app/controllers/concerns/exception_filter.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module ExceptionFilter
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordInvalid,
+                with: ->(e) { log_error(e) && render_api_error(APIError::RecordInvalidError.new(e.record.errors)) }
+
+    rescue_from ActiveRecord::RecordNotFound,
+                with: ->(e) { log_error(e) && render_api_error(APIError::NotFoundError.new) }
+
+    rescue_from APIError::StandardError,
+                with: ->(e) { log_error(e) && render_api_error(e) }
+  end
+
+  private
+
+  def log_error(error)
+    Rails.logger.error(error)
+  end
+
+  def render_api_error(error)
+    render json: error, status: error.status
+  end
+end
